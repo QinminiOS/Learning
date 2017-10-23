760f263fca5565842db36fafde583de01f12c7f6
qinmin <544652424@qq.com>
2017-10-24 07:39:39 +0800

    二叉树

--- 算法/算法/tree.mm
+++ 算法/算法/tree.mm
@@ -10,10 +10,14 @@
 
 
 /**
- 先序遍历
+     先序遍历
  */
 void preVisit(Tree *root)
 {
+    if (root == NULL) {
+        return;
+    }
+    
     stack<Tree *> s;
     s.push(root);
     
@@ -34,53 +38,79 @@ void preVisit(Tree *root)
 }
 
 /**
- 中序遍历
+     中序遍历
  */
 void inVisit(Tree *root)
 {
-    stack<Tree *> s;
-    s.push(root);
+    if (root == NULL) {
+        return;
+    }
     
-    while (s.size() > 0) {
-        Tree *t = s.top();
-        s.pop();
-        
-        printf("%d ", t->value);
-        
-        if (t->right) {
-            s.push(t->right);
+    stack<Tree *> s;
+    Tree *p = root;
+    
+    while (p) {
+        while (p) {
+            s.push(p);
+            p = p->left;
         }
         
-        if (t->left) {
-            s.push(t->left);
-        }
+        do {
+            Tree *tmp = s.top();
+            s.pop();
+            printf("%d ", tmp->value);
+            p = tmp->right;
+        } while (!p && !s.empty());
     }
 }
 
 
 /**
- 
+     后序遍历
  */
 void postVisit(Tree *root)
 {
-    stack<Tree *> s;
-    s.push(root);
+    if (root == NULL) {
+        return;
+    }
     
-    while (s.size() > 0) {
-        Tree *t = s.top();
+    stack<Tree *> s;
+    Tree *p = root;
+    Tree *lastVisit = NULL;
+
+    while (p) {
+        s.push(p);
+        p = p->left;
+    }
+    
+    while (!s.empty()) {
+        p = s.top();
         s.pop();
         
-        if (t->right) {
-            s.push(t->right);
+        // 一个根节点被访问的前提是:无右子树或右子树已被访问过
+        if (p->right == NULL || p->right == lastVisit) {
+            printf("%d ", p->value);
+            lastVisit = p;
+            
+        //若左子树刚被访问过，则需先进入右子树,根节点需再次入栈
+        }else {
+            s.push(p);
+            p = p->right;
+            while (p) {
+                s.push(p);
+                p = p->left;
+            }
         }
-        
-        if (t->left) {
-            s.push(t->left);
-        }
-        
-        printf("%d ", t->value);
     }
+    
 }
+
+/**
+      1
+   2     3
+ 4   5  6   7
+
+ */
 
 int main(int argc, const char * argv[])
 {
